51911651c898aad812ad591eda6ea9b807ff74fe
santi vittor <santi.vittor-@live.com.ar>
2021-09-02 17:46:06 -0300

    Online Resume de Santiago Vittor V1.0.1

---
+++ script.js
@@ -0,0 +1,11 @@
+let contenedorTop = document.getElementById("topContainer")
+let contenedorBottom = document.getElementById("bottomContainer")
+
+
+function aparecer(whichOne){
+    whichOne.style.opacity="1";
+    whichOne.style.transition="3s";
+}
+
+aparecer(contenedorTop)
+aparecer(contenedorBottom)
